f9fd99c4dc19c258bc03bf491814c9744a21e07d
web-dev <raghavbhatnagar@gmail.com>
2026-07-30 14:40:15 +0530

    modified

--- demo3.js
+++ demo3.js
@@ -10,3 +10,12 @@ const{name : newname,age,branch}=student;
 console.log("name",newname);
 console.log("age",age);
 console.log("branch",branch);
+const num=[11,22,33];
+//const a=num[0];
+//const b=num[1];
+//const c=num[2];
+//array destructing
+const[a,b,c]=num;
+console.log("a=",a);
+console.log("b=",b);
+console.log("c=",c);
